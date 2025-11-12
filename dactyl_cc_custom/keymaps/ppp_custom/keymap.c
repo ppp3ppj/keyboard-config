f1bb3a96cbc711e57784b86e1658910b26e194d8
ppp3ppj <ppp.3ppj@gmail.com>
2025-11-12 20:20:11 +0700

    Update info new 1 key for both side left and right under shift key

--- dactyl_cc_custom/keymaps/ppp_custom/keymap.c
+++ dactyl_cc_custom/keymaps/ppp_custom/keymap.c
@@ -19,7 +19,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,   KC_E,    KC_R,    KC_T,                                         KC_Y, KC_U,    KC_I,    KC_O,   KC_P,    KC_BSLS,
         KC_ESC, KC_A,    KC_S,   KC_D,    KC_F,    KC_G,                                         KC_H, KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,   KC_C,    KC_V,    KC_B,                                         KC_N, KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
-                 KC_GRV, KC_CAPS, KC_LEFT, KC_RIGHT,                                                     KC_UP, KC_DOWN, KC_LBRC,  KC_RBRC,
+         KC_LSFT, KC_GRV, KC_CAPS, KC_LEFT, KC_RIGHT,                                                     KC_UP, KC_DOWN, KC_LBRC,  KC_RBRC, KC_RSFT,
                                                           KC_LCTL,  KC_LALT,     KC_RALT, KC_RCTL,
                                                  KC_BSPC, KC_DEL, KC_LGUI,     KC_RGUI, KC_ENT,  KC_SPC,
                                                                    MO(NAV),     MO(NAV)
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, _______, _______,                                         _______, _______,   _______,  _______,   _______, _______,
         _______, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_PGUP,                                         KC_PGDN, KC_RSFT,   KC_RCTL,  KC_RALT,   KC_RGUI, _______,
         _______,   KC_NUM,   _______,   _______,   _______,   _______,                                           _______, _______, _______,  _______, KC_ENT, _______,
-                 _______,   _______,   _______,   _______,                                                             _______,   _______, _______,  _______,
+        _______, _______,   _______,   _______,   _______,                                                             _______,   _______, _______,  _______, _______,
                                                               KC_HOME, KC_ESC,     KC_ESC, KC_END,
                                                      _______, _______, KC_PGUP,     KC_PGDN,  _______, _______,
                                                                        _______,     _______
