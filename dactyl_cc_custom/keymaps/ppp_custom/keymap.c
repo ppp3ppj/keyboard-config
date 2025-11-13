c1f3d06e57415f7f9cf76c1bfede7e85d0e77d84
ppp3ppj <ppp.3ppj@gmail.com>
2025-11-13 21:40:14 +0700

    Update keymap for support new column and 3 key in this column each side

--- dactyl_cc_custom/keymaps/ppp_custom/keymap.c
+++ dactyl_cc_custom/keymaps/ppp_custom/keymap.c
@@ -15,9 +15,9 @@ enum layers {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT(
-        KC_EQL,  KC_1,    KC_2,   KC_3,    KC_4,    KC_5,                                         KC_6, KC_7,    KC_8,    KC_9,   KC_0,    KC_MINS,
-        KC_TAB,  KC_Q,    KC_W,   KC_E,    KC_R,    KC_T,                                         KC_Y, KC_U,    KC_I,    KC_O,   KC_P,    KC_BSLS,
-        KC_ESC, KC_A,    KC_S,   KC_D,    KC_F,    KC_G,                                         KC_H, KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT,
+        KC_EQL,  KC_1,    KC_2,   KC_3,    KC_4,    KC_5, KC_5,                                   KC_6, KC_7,    KC_8,    KC_9,   KC_0,    KC_MINS, KC_6,
+        KC_TAB,  KC_Q,    KC_W,   KC_E,    KC_R,    KC_T, KC_5,                                   KC_Y, KC_U,    KC_I,    KC_O,   KC_P,    KC_BSLS, KC_6,
+        KC_ESC, KC_A,    KC_S,   KC_D,    KC_F,    KC_G,  KC_5,                                   KC_H, KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT, KC_6,
         KC_LSFT, KC_Z,    KC_X,   KC_C,    KC_V,    KC_B,                                         KC_N, KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
          KC_LSFT, KC_GRV, KC_CAPS, KC_LEFT, KC_RIGHT,                                                     KC_UP, KC_DOWN, KC_LBRC,  KC_RBRC, KC_RSFT,
                                                           KC_LCTL,  KC_LALT,     KC_RALT, KC_RCTL,
@@ -25,9 +25,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                    MO(NAV),     MO(NAV)
     ),
     [NAV] = LAYOUT(
-        KC_F1, KC_F2,   KC_F3, KC_F4, KC_F5, KC_F6,                                           KC_F7, KC_F8,   KC_F9,  KC_F10,   KC_F11,   KC_F12,
-        _______, _______, _______, _______, _______, _______,                                         _______, _______,   _______,  _______,   _______, _______,
-        _______, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_PGUP,                                         KC_PGDN, KC_RSFT,   KC_RCTL,  KC_RALT,   KC_RGUI, _______,
+        KC_F1, KC_F2,   KC_F3, KC_F4, KC_F5, KC_F6,     _______,                                  KC_F7, KC_F8,   KC_F9,  KC_F10,   KC_F11,   KC_F12, KC_6,
+        _______, _______, _______, _______, _______, _______,   _______,                                      _______, _______,   _______,  _______,   _______, _______, KC_6,
+        _______, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_PGUP,    _______,                                     KC_PGDN, KC_RSFT,   KC_RCTL,  KC_RALT,   KC_RGUI, _______, KC_6,
         _______,   KC_NUM,   _______,   _______,   _______,   _______,                                           _______, _______, _______,  _______, KC_ENT, _______,
         _______, _______,   _______,   _______,   _______,                                                             _______,   _______, _______,  _______, _______,
                                                               KC_HOME, KC_ESC,     KC_ESC, KC_END,
